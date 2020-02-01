68198cc082446ce1f4127973e0e05e13b7abdc69
dionwave <wangdion@hotmail.ca>
2020-02-01 13:25:27 -0500

    Dion
    Dion

---
+++ Assets/Scripts/SpawnBrokenPieces.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBrokenPieces : MonoBehaviour
+{
+    public GameObject[] brokenPieces;
+    public int maxPieces = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            int piecesToDrop = Random.Range(1, maxPieces);
+            for(int i= 0; i < piecesToDrop; i++)
+            {
+                int randomPiece = Random.Range(0, brokenPieces.Length);
+
+                Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealth : MonoBehaviour
+{
+    public static UIHealth instance;
+    public Slider healthSlider;
+    public Text healthText;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
