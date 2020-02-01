cfce7dc8b795dca29e27ed4cb5bbd80ceb770bd7
BAT-COMPUTER\Nathan <nathanmladd@gmail.com>
2020-02-01 15:41:49 -0500

    Robot Attack base. Incomplete.

---
+++ Assets/NathanWork/N_scripts/AnimationEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+
+    public void punchActive()
+    {
+        print("Punch is active");
+    }
+
+    public void punchInActive()
+    {
+        print("Punch cooldown");
+    }
+
+
+}
---
+++ Assets/NathanWork/N_scripts/RobotAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAttack : MonoBehaviour
+{
+    //Punch
+    //Kick
+    //Missiles
+    //Targeting
+    public Animator robotAnimator;
+    public int punchDamage = 3;
+    public int kickDamage = 2;
+    public bool isPlayerOne = true;
+
+    void Start()
+    {
+        robotAnimator = robotAnimator.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && isPlayerOne)
+        {
+            robotAnimator.SetTrigger("punch");
+            // if active frames & if collision then deal damage
+
+        }
+
+
+    }
+}
