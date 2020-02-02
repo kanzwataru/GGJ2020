166fe2443ea774254331d3932c52c275d2c86b51
神崎航 <kanzwataru@gmail.com>
2020-02-02 11:27:36 -0500

    * do some stuff

--- Assets/Game/Scripts/RepairmanAnimCoordinator.cs
+++ Assets/Game/Scripts/RepairmanAnimCoordinator.cs
@@ -10,12 +10,16 @@ public class RepairmanAnimCoordinator : MonoBehaviour
     LadderMovement ladderMove;
     Animator       animator;
 
+    float lastXPos;
+
     // Start is called before the first frame update
     void Start()
     {
         motor = GetComponent<RepairmanMotor>();
         ladderMove = GetComponent<LadderMovement>();
         animator = visuals.GetComponent<Animator>();
+
+        lastXPos = transform.localPosition.x;
     }
 
     // Update is called once per frame
@@ -24,5 +28,16 @@ public class RepairmanAnimCoordinator : MonoBehaviour
         animator.SetBool("isRunning", motor.IsRunning());
         animator.SetBool("isJumping", motor.IsJumping());
         animator.SetBool("isClimbing", ladderMove.OnLadder());
+
+        float xPos = transform.localPosition.x;
+
+        if(xPos > lastXPos) {
+            ladderMove.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(xPos < lastXPos) {
+            ladderMove.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        lastXPos = xPos;
     }
 }
