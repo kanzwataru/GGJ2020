bea7452ef63cb537032390270c2bd339a0a03ab1
Nathan Ladd <nathanmladd@gmail.com>
2020-02-02 12:47:59 -0500

    Animation starting for the chicken

--- Assets/NathanWork/N_scripts/Robot.cs
+++ Assets/NathanWork/N_scripts/Robot.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     public BoxCollider boxCollider;
+    public Animator chickenAnimator;
 
     private int health;
+    public float speed = 1.5f;
 
     void Start()
     {
@@ -20,7 +22,27 @@ public class Robot : MonoBehaviour
             print("Hit!");
             health -= GameLogic.punchDamage;
         }
-            
+    }
+
+    private void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (inputH * speed * Time.deltaTime), transform.position.y);
+
+        if (inputH != 0)
+        {
+            chickenAnimator.SetBool("isWalking", true);
+        } else
+        {
+            chickenAnimator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            chickenAnimator.SetTrigger("jump");
+        }
+
     }
 
 }
