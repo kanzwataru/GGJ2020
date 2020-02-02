60f8755d3b5c2d84e158bfd009d3401436f69698
BAT-COMPUTER\Nathan <nathanmladd@gmail.com>
2020-02-01 20:21:37 -0500

    Repair game in room

--- Assets/NathanWork/N_scripts/Room.cs
+++ Assets/NathanWork/N_scripts/Room.cs
@@ -6,19 +6,52 @@ public class Room : MonoBehaviour
 {
     [HideInInspector] public BoxCollider roomCollider;
     [HideInInspector] public SpriteRenderer target;
-    public GameObject repairGame;
+    public Transform repairGuide;
+    private GameObject repairGame;
+    private bool inRoom = false;
+
     public bool roomHit = false;
 
     private void Start()
     {
         roomCollider = GetComponent<BoxCollider>();
         target = GetComponentInChildren<SpriteRenderer>();
+        repairGuide = repairGuide.GetComponent<Transform>();
+        repairGame = GameObject.Find("RepairGame");
+        repairGame.GetComponent<RepairRing>().success.AddListener(roomFixed);
     }
 
+    private void roomFixed()
+    {
+        // only if still in room
+        if (inRoom)
+        {
+            roomHit = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            print("Entered!");
+            inRoom = true;
+            if (roomHit)
+            {
+                print("Repair Game...");
+                repairGame.transform.position = repairGuide.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("Left");
+            inRoom = false;
+            repairGame.GetComponent<RepairRing>().resetPosition();
+        }
     }
 
 }
