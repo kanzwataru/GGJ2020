0cf6e66083b58abfd530d3cd6ad0957c7da477b1
dionwave <wangdion@hotmail.ca>
2020-02-02 12:39:57 -0500

    Dion's
    Dion's

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public float time;
+    public GameObject fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void NextScene()
+    {
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        fade.SetActive(true);
+        yield return new WaitForSeconds(time);
+        
+        SceneManager.LoadScene("UIScene");
+    }
+}
